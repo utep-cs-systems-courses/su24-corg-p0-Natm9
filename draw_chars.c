abff5332283c245b5b5f70bdba26255f45f8bc64
Natm9 <nterrazasm@miners.utep.edu>
2024-06-14 11:23:43 -0600

    update 2.0

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,16 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+void print_char_11x16(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 11; row++){
+    unsigned short rowBits = font_11x16[c][row];
+    for (char col = 0; col < 16; col++){
+      unsigned short colMask = 1 << (15-col); /* mask to solect bit associated bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
