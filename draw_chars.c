383b87de05bf610b0c89d7c74231c30736f94486
Natm9 <nterrazasm@miners.utep.edu>
2024-06-17 08:35:24 -0600

    Update 3.0

--- draw_chars.c
+++ draw_chars.c
@@ -18,12 +18,14 @@ void print_char_5x7(char c)
 void print_char_11x16(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 11; row++){
-    unsigned short rowBits = font_11x16[c][row];
-    for (char col = 0; col < 16; col++){
-      unsigned short colMask = 1 << (15-col); /* mask to solect bit associated bit */
+  for (char row = 0; row < 16; row++){ // Made changes
+    // unsigned short rowBits = font_11x16[(unsigned char)c][row];
+    for (char col = 0; col < 11; col++){ // Made changes
+      unsigned short rowBits = font_11x16[(unsigned char)c][col]; // Made changes
+      unsigned short colMask = 1 << row; /* mask to solect bit associated bit */ // made changes
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
-    putchar('\n');
+    if (row < 15)
+      putchar('\n');
   }
 }
